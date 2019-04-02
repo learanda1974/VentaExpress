6fb99bced43b9e5ab8949c0551601c2103dbebae
cetecom <cetecom@LCV10807>
2019-04-01 22:30:55 -0300

    Actualizacion Menu

--- lib/menu.php
+++ lib/menu.php
@@ -2,19 +2,19 @@
 
 <div class="list-group">
 
-<a class="list-group-item" data-remote="true" href="#sub_categoria_4" id="categoria_4" data-toggle="collapse" data-parent="#sub_categoria_4" style="padding-left: 25px;">
+    <a class="list-group-item" data-remote="true" href="#sub_categoria_4" id="categoria_4" data-toggle="collapse" data-parent="#sub_categoria_4" style="padding-left: 25px;">
           <span class="fa fa-shopping-cart fa-lg fa-fw"></span>
           <span style="margin-left: 25px;">Venta Express</span>
           <span class="menu-ico-collapse"><i class="fa fa-chevron-down"></i></span>
-        </a>
+    </a>
 
         <div class="collapse list-group-submenu" id="sub_categoria_4">
           
           <a data-remote="true" href="#sub_categoria_2" class="list-group-item sub-item" data-parent="#sub_categoria_4" data-parent="#sub_categoria_2" style="padding-left: 78px;">
-                <span style="margin-left: 25px;">Ingreso</span>
-                <span class="menu-ico-collapse"><i class="fa fa-chevron-down"></i></span>
+                Ingreso
+                <span class="menu-ico-collapse"><i class="fa fa-chevron-down"></i></span>                
           </a>
-          
+            
             <div class="collapse list-group-submenu" id="sub_categoria_2">
                 <a href="#" class="list-group-item sub-item" data-parent="#sub_categoria_2" style="padding-left: 78px;">Producto</a>
                 <a href="#" class="list-group-item sub-item" data-parent="#sub_categoria_2" style="padding-left: 78px;">Venta</a>          
